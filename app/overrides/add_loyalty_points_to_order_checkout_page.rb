4d58836bd4457b985f1bf30bbbdbbdb73e6891a3
ariuslee <ariusleechihin@gmail.com>
2018-02-06 17:01:47 +0800

    Hotfix: Remove mni points line in order show page if not paid.

--- app/overrides/add_loyalty_points_to_order_checkout_page.rb
+++ app/overrides/add_loyalty_points_to_order_checkout_page.rb
@@ -2,24 +2,26 @@ Deface::Override.new(virtual_path: 'spree/shared/_order_details',
   name: 'add_loyalty_points_to_order_checkout_page',
   insert_after: "#order-charges",
   text: "
-    <% if spree_current_user %>
-      <tfoot id='loyalty_points'>
-        <tr id='loyalty-points-row'>
-          <% if @order.loyalty_points_awarded? %>
+    <% if @order.payment_state && @order.payment_state != 'balance_due' %>
+      <% if spree_current_user %>
+        <tfoot id='loyalty_points'>
+          <tr id='loyalty-points-row'>
+            <% if @order.loyalty_points_awarded? %>
+              <td></td>
+              <td colspan='4' class='total'><span><%= @order.loyalty_points_for(@order.item_total) %></span> <b><%= Spree.t(:loyalty_points) %> have been credited to your account.</b></td>
+            <% else %>
+              <td></td>
+              <td colspan='4' class='total'><span><%= @order.loyalty_points_for(@order.item_total) %></span> <b><%= Spree.t(:loyalty_points) %> will be credited to your account  soon.</b></td>
+            <% end %>
+          </tr>
+        </tfoot>
+      <% else %>
+        <tfoot id='loyalty_points'>
+          <tr id='loyalty-points-row'>
             <td></td>
-            <td colspan='4' class='total'><span><%= @order.loyalty_points_for(@order.item_total) %></span> <b><%= Spree.t(:loyalty_points) %> have been credited to your account.</b></td>
-          <% else %>
-            <td></td>
-            <td colspan='4' class='total'><span><%= @order.loyalty_points_for(@order.item_total) %></span> <b><%= Spree.t(:loyalty_points) %> will be credited to your account  soon.</b></td>
-          <% end %>
-        </tr>
-      </tfoot>
-    <% else %>
-      <tfoot id='loyalty_points'>
-        <tr id='loyalty-points-row'>
-          <td></td>
-          <td colspan='4' class='total'><b><%= link_to 'Sign up now', spree.signup_path %></b> to receive Mini Points from purchases!</td>
-        </tr>
-      </tfoot>
+            <td colspan='4' class='total'><b><%= link_to 'Sign up now', spree.signup_path %></b> to receive Mini Points from purchases!</td>
+          </tr>
+        </tfoot>
+      <% end %>
     <% end %>
   ")
